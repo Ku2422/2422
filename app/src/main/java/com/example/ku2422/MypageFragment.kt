23d57405cd5e6073287fcdf2050956b5fd10de19
eigenkyeong <kokoyou163@gmail.com>
2022-06-07 04:19:29 +0900

    Feat: search review and sort

--- app/src/main/java/com/example/ku2422/MypageFragment.kt
+++ app/src/main/java/com/example/ku2422/MypageFragment.kt
@@ -27,7 +27,9 @@ class MypageFragment : Fragment() {
     private lateinit var adapter: ReviewListAdapter
     private lateinit var layoutManager: RecyclerView.LayoutManager
     lateinit var userManager: LoginRepository
+    lateinit var userId: String
     var reviewData: ArrayList<Store> = ArrayList()
+    var searchData: ArrayList<Store> = ArrayList()
     var data = mutableListOf<Store>()
 
     companion object{
@@ -45,32 +47,20 @@ class MypageFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentMypageBinding.inflate(inflater, container, false)
 
-//        userManager = LoginRepository
+//        userId = GlobalApplication.getInstance().getValue("userId")!!
+//        StoreDB.getStoreById("userId") {
+//            reviewData.add(it)
+//        }
 
         // get reviewData
-        reviewData.add(Store("2258663590","","user1","건대카페","말차라떼", 4500,"so good", 5.0,"2022-01-01", 887789f,123123f)) //temp
-//        reviewData = StoreDB.getStoreById()
+        reviewData.add(Store("2258663590","","user1","건대카페","말차라떼",
+            4500,"so good", 5.0,"2022-01-01", 887789f,123123f)) //temp
+        reviewData.add(Store("2258663590","","user1","레스티오","아이스티",
+            4000,"sooooo good", 4.0,"2022-03-01", 887789f,123123f)) //temp
+        reviewData.add(Store("2258663590","","user1","건대식당","감자탕",
+            9000,"good", 5.0,"2022-02-15", 887789f,123123f)) //temp
+
         adapter = ReviewListAdapter(reviewData)
-
-        // Search review
-        binding.apply {
-            btnMypageSearch.setOnClickListener {
-                val search = editSearch.text.toString()
-                if (search == null) {
-                    // get reviewData
-                }
-                else {
-                    for (review in reviewData) {
-                        if (search == review.storeName) {
-                            reviewData.clear()
-                            reviewData.add(review)
-
-                        }
-                    }
-                }
-                adapter.notifyDataSetChanged()
-            }
-        }
 
         // To more review
         adapter.itemClickListener = object: ReviewListAdapter.OnItemClickListener {
@@ -82,12 +72,30 @@ class MypageFragment : Fragment() {
                 transaction.commit()
             }
         }
-
-
         binding.recyclerReview.adapter = adapter
 
         layoutManager = LinearLayoutManager(activity)
         binding.recyclerReview.layoutManager = layoutManager
+
+        // Search review
+        binding.apply {
+            btnMypageSearch.setOnClickListener {
+                val search = editSearch.text?.toString()
+                if (search == "") {
+                    adapter.searchItem(reviewData)
+                }
+                else {
+                    if(!searchData.isEmpty())
+                        searchData.clear()
+                    for (review in reviewData) {
+                        if (review.storeName.contains(search.toString())) {
+                            searchData.add(review)
+                        }
+                    }
+                    adapter.searchItem(searchData)
+                }
+            }
+        }
 
         initSpinner()
 
@@ -95,29 +103,30 @@ class MypageFragment : Fragment() {
     }
 
     private fun initSpinner() {
+
         val spinner_list = resources.getStringArray(R.array.spinner_list)
-        val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_dropdown_item, spinner_list)
+        val spinneradapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            spinner_list
+        )
         binding.apply {
-            spinner.adapter = adapter
+            spinner.adapter = spinneradapter
             spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
-                    when(position) {
-                        0 -> {
-                            reviewData = ArrayList(reviewData.sortedBy { it.date })
-                        }
-                        1 -> {
-                            reviewData = ArrayList(reviewData.sortedBy { it.star })
-                        }
-                        2 -> {
-                            // 거리순
-                        }
-                    }
+                override fun onItemSelected(
+                    p0: AdapterView<*>?,
+                    p1: View?,
+                    position: Int,
+                    p3: Long
+                ) {
+                    adapter.sortItem(position)
 
                 }
 
                 override fun onNothingSelected(p0: AdapterView<*>?) {
 
                 }
+
             }
         }
     }
