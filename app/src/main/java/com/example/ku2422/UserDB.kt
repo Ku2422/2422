046a9f5b8ab0336d62587ba0b9980f1b9a2836a6
yoonhyeokCho <hyeok6640@gmail.com>
2022-06-08 07:07:02 +0900

    SocialFragment 친구정보 가져오기

--- app/src/main/java/com/example/ku2422/UserDB.kt
+++ app/src/main/java/com/example/ku2422/UserDB.kt
@@ -9,6 +9,11 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
 
 object UserDB {
     val userRdb: DatabaseReference = Firebase.database.getReference("UserDB")
